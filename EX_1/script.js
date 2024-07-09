666094c3791eb57e63d7892dd3e8781426e4652e
Mohamed Reda Foshi <reda.foshi11@gmail.com>
2024-07-10 00:34:07 +0100

    creat a javascript repo

---
+++ EX_1/script.js
@@ -0,0 +1,29 @@
+
+let firstnumber=document.getElementById("firstnumber");
+let secondnumber=document.getElementById("secondnumber");
+let Resulte=document.getElementById("Resulte");
+
+function one(){
+
+Resulte.innerText=parseInt(firstnumber.value)+parseInt(secondnumber.value);
+
+}
+
+
+function tow(){
+
+    Resulte.innerText=parseInt(firstnumber.value)-parseInt(secondnumber.value);
+    
+    }
+
+    function tree(){
+
+        Resulte.innerText=parseInt(firstnumber.value)*parseInt(secondnumber.value);
+        
+        }
+
+    function four(){
+            Resulte.innerText=parseInt(firstnumber.value)/parseInt(secondnumber.value);            
+    }
+
+
---
+++ test_1.js
@@ -0,0 +1,61 @@
+/*
+const obj={
+    name:"dog",
+    leg:"4",
+    tails:"1",
+    friends:"2",
+
+};
+
+*/
+
+/*
+let y=3;
+        
+if(y==5){
+    console.log("tva is 15%");
+}
+else if(y==3){
+    console.log("tva is 25%");
+}
+else if(y==4){
+    console.log("tva is 20%");
+}
+let i;
+for( i=0;i<=10;i++){
+    console.log(i);
+}
+
+
+const obj={
+    name:"reda",
+    age:"20",
+    ["last name"]:"foshi",
+};
+
+console.log(obj ["last name"])
+*/
+
+const myArray = [];
+
+
+for(let i= 10;i>0;i=i-10){
+  
+
+  console.log(myArray.push(i))
+}
+/*
+
+let obj_1={
+
+        fname:"reda",
+        lname:"foshi",
+        age:"20",
+        height:"177",
+};
+
+
+console.log("dog name",obj.name)
+console.log("friends number",obj.friends)
+console.log("man age",obj_1.age)
+*/
